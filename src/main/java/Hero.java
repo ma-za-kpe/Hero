a66ce8f4b48128cb9116303dc349e04032f7c149
ma-za-kpe <makpalyy@gmail.com>
2019-04-12 14:39:10 +0200

    add all hero test and methods

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,3 +1,54 @@
+import java.util.ArrayList;
+
 public class Hero {
+    private String name;
+    private String specialSkill;
+    private String weakness;
+    private int age;
+    private static ArrayList<Hero> instances = new ArrayList<>(); // I’m new. When do I get created?
+    private int id;
+
+    public Hero (String name, String specialSkill, String weakness, int age){
+    this.name = name;
+    this.specialSkill = specialSkill;
+    this.weakness = weakness;
+    this.age =age;
+    instances.add(this);
+    this.id = instances.size();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecialSkill() {
+        return specialSkill;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    //return all heroes
+    public static ArrayList<Hero> getAllHeroes(){
+        return instances;
+    }
+
+    public static void clearAllHeroes(){
+        instances.clear();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Hero findById(int id){
+        return instances.get(id-1); //why minus 1? See if you can figure it out.
+    //id-1 stands for the index of the element in the arraylist while id is the element in the array(the instance.size())
+    }
 
 }
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,5 +1,86 @@
+import org.junit.After;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HeroTest {
+
+    @Test
+    public void NewHeroObjectGetsCorrectlyCreated_true() throws Exception {
+        Hero post = new Hero("Iron Man", "flying", "water", 23);
+        assertEquals(true, post instanceof Hero);
+    }
+
+    @Test
+    public void HeroInstantiatesWithName_true() throws Exception {
+        Hero post = new Hero("Iron Man", "flying", "water", 23);
+        assertEquals("Iron Man", post.getName());
+
+    }
+
+    @Test
+    public void HeroInstantiatesWithspecialSkill_true() throws Exception {
+        Hero post = new Hero("Iron Man", "flying", "water", 23);
+        assertEquals("flying", post.getSpecialSkill());
+
+    }
+
+    @Test
+    public void HeroInstantiatesWithWeakness_true() throws Exception {
+        Hero post = new Hero("Iron Man", "flying", "water", 23);
+        assertEquals("water", post.getWeakness());
+
+    }
+
+    @Test
+    public void HeroInstantiatesWithAge_true() throws Exception {
+        Hero post = new Hero("Iron Man", "flying", "water", 23);
+        assertEquals(23, post.getAge());
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Hero.clearAllHeroes(); //clear out allll the posts before each test.
+    }
+
+    @Test
+    public void AllPostsAreCorrectlyReturned_true() throws Exception {
+        Hero hero = new Hero("Iron Man", "flying", "water", 23);
+        Hero otherHero = new Hero ("Spiderman", "flying", "water", 23);
+        assertEquals(2, Hero.getAllHeroes().size());
+    }
+
+    @Test
+    public void AllPostsContainsAllPosts_true() {
+        Hero hero = new Hero("Iron Man", "flying", "water", 23);
+        Hero otherHero = new Hero ("Spiderman", "flying", "water", 23);
+        assertTrue(Hero.getAllHeroes().contains(hero));
+        assertTrue(Hero.getAllHeroes().contains(otherHero));
+    }
+
+    @Test
+    public void getId_postsInstantiateWithAnID_1() throws Exception{
+        Hero.clearAllHeroes();  // Remember, the test will fail without this line! We need to empty leftover Posts from previous tests!
+        Hero myPost = new Hero("Iron Man", "flying", "water", 23);
+        assertEquals(1, myPost.getId());
+    }
+
+    public Hero setupNewPost(){
+        return new Hero("Iron Man", "flying", "water", 23);
+    }
+
+    @Test
+    public void findReturnsCorrectPost() throws Exception {
+        Hero post = setupNewPost();
+        assertEquals(1, Hero.findById(post.getId()).getId());
+    }
+
+    @Test
+    public void findReturnsCorrectPostWhenMoreThanOnePostExists() throws Exception {
+        Hero hero = setupNewPost();
+        Hero otherHero = new Hero("Spiderman", "flying", "water", 23);
+        assertEquals(2, Hero.findById(otherHero.getId()).getId());
+    }
 
 }
