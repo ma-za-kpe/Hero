c228e165966e2613e0fb2b9023d709c1df7ccff0
ma-za-kpe <makpalyy@gmail.com>
2019-04-12 16:54:59 +0200

    add sqaud test and methods

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,3 +1,52 @@
+import java.util.ArrayList;
+
 public class Squad {
+
+    private String name;
+    private String cause;
+    private static ArrayList<Squad> instances = new ArrayList<>(); // I’m new. When do I get created?
+    private int id;
+
+    public Squad(String name, String cause) {
+        this.name = name;
+        this.cause = cause;
+        instances.add(this);
+        this.id = instances.size();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //return all heroes
+    public static ArrayList<Squad> getAllSquad(){
+        return instances;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public static void clearAllSquad(){
+        instances.clear();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Squad findById(int id){
+        return instances.get(id-1); //why minus 1? See if you can figure it out.
+        //id-1 stands for the index of the element in the arraylist while id is the element in the array(the instance.size())
+    }
+
+    public void update(String name, String cause) {
+        this.name = name;
+        this.cause = cause;
+    }
+
+    public void deleteSquad(){
+        instances.remove(id-1); //same reason
+    }
 
 }
