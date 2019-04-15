c71a9fdac170d304542533b798ae111e4ed8a43b
ma-za-kpe <makpalyy@gmail.com>
2019-04-15 10:19:03 +0200

    add port configuration in app file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,6 +18,16 @@ public class App {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        setPort(port);
+
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             ArrayList<Squad> squads = Squad.getAllSquad();
